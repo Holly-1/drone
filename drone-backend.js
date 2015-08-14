a9f00183d1473274abfdb846666cf682bf0a199e
V5798 (Course ID) <v5798@cam.ac.uk>
2015-08-14 11:19:49 +0100

    Drone 11.20

--- drone-backend.js
+++ drone-backend.js
@@ -11,11 +11,66 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
     
 // Fly the bot
-function fly(robot) {
 
+function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+
+    bot.drone.config('general:navdata_demo', 'TRUE');
+
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    //tell drone it's on the ground
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+
+    after(2*1000, function() {
+        bot.drone.left(0.15);
+    })
+    after(3*1000, function() {
+        bot.drone.left(0);
+        bot.drone.forward(0.15)
+    })
+    after(4*1000, function() {
+        bot.drone.forward(0);
+        bot.drone.right(0.15)
+    })
+    after(5*1000, function() {
+        bot.drone.right(0);
+        bot.drone.back(0.15)
+    })
+
+
+
+    after(6*1000, function() {
+        bot.drone.back(0);
+        bot.drone.land();
+    });
+    after(12*1000, function() {
+        bot.drone.stop();
+    });
 }
+
 
 Cylon.start();
--- drone-frontend.js
+++ drone-frontend.js
@@ -6,7 +6,14 @@ function init() {
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-    
+
+    // We only want rectangles that have each dimension no smaller than 20 pixels
+    // tracker.setMinDimension(20);
+
+    // The colors we're interested in
+    TrackerUtils.addTrackingColor("#A94A45", "red", tracker);
+    TrackerUtils.addTrackingColor("#5EA24E", "green", tracker);
+    TrackerUtils.addTrackingColor("#CB7F84", "magenta", tracker);
     TrackerUtils.startTrackingColors(tracker);
 
     // Whenever there is a new color detected, mark them
@@ -16,5 +23,34 @@ function initTracker(element) {
 
     return tracker;
 }
+
+
+function markColors(colors, element) {
+    // Do the marking
+    // Remove previously drawn rectangles indicating detected colors
+    // Get the drawing surface
+    var canvas = $(element + ' .canvas').get(0);
+    var context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, context.width, context.height);
+    for (var i = 0; i < colors.length; i++) {
+        drawRectangle(colors[i], context);
+        writeRectangle(colors[i], element + " .output");
+    }
+
+    function drawRectangle(rect, context) {
+        context.strokeStyle = rect.color;
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    function writeRectangle(rect, element) {
+        $(element)
+            .append("<p>")
+            .append(rect.color + ": " + rect.width + "X" + rect.height)
+            .append(" @ " + rect.x + ":" + rect.y)
+    }
+
+}
+
 
 window.addEventListener("load", init);
